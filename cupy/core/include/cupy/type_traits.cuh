ee54150ade8fee62ef57a1dc25e2aa331a782b81
Leo Fang <leofang@bnl.gov>
2020-07-09 22:14:11 -0400

    clean up type_traits a bit to prepare for C++11 support

--- cupy/core/include/cupy/type_traits.cuh
+++ cupy/core/include/cupy/type_traits.cuh
@@ -1,11 +1,24 @@
-// Implements a casting function to make it compatible with scipy
-// Use like cast<to_type>(value)
-// It's actually really simple - most of this is <type_traits>
-// Small bit of <type_traits> which cannot be imported in NVRTC
-// Requires compiling with --std=c++11 or higher
-// Author: Jeffrey Bush (@coderforlife)
+/*
+ * Small bit of <type_traits> which cannot be imported in NVRTC
+ * Requires compiling with --std=c++11 or higher
+ *
+ * Author: Jeffrey Bush (@coderforlife)
+*/
 
 // TODO(leofang): integrate with jitify support (#3408)
+
+
+// _MSVC_LANG is only defined in MSVC 2015U3+.
+#if __cplusplus >= 201103L \
+    || (defined(_MSVC_LANG) && _MSVC_LANG >= 201103L)  // C++11 support
+#define CUPY_CPP11_supported 1
+#else
+#define CUPY_CPP11_supported 0
+#endif
+
+
+namespace cupy {
+
 template<bool B, class T=void> struct enable_if {};
 template<class T> struct enable_if<true, T> { typedef T type; };
 template<class T> struct remove_const          { typedef T type; };
@@ -15,10 +28,8 @@ template<class T> struct remove_volatile<volatile T> { typedef T type; };
 template<class T> struct remove_cv {
   typedef typename remove_volatile<typename remove_const<T>::type>::type type;
 };
-template<class T, T v>
-struct integral_constant { static constexpr T value = v; };
-//struct true_type { static constexpr bool value = true; };
-//struct false_type { static constexpr bool value = false; };
+
+template<class T, T v> struct integral_constant { static constexpr T value = v; };
 typedef integral_constant<bool, true> true_type;
 typedef integral_constant<bool, false> false_type;
 template<class T> struct __is_fp : public false_type {};
@@ -29,10 +40,5 @@ template<class T> struct is_floating_point
     : public __is_fp<typename remove_cv<T>::type> {};
 template<class T> struct is_signed : integral_constant<bool, (T)(-1)<0> {};
 template <typename B, typename A>
-__device__
-typename enable_if<!is_floating_point<A>::value||is_signed<B>::value, B>::type
-cast(A a) { return (B)a; }
-template <typename B, typename A>
-__device__
-typename enable_if<is_floating_point<A>::value&&!is_signed<B>::value, B>::type
-cast(A a) { return (a >= 0) ? (B)a : -(B)(-a); }
+
+}  // namespace cupy
